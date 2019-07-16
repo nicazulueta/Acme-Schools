60cab5d98bcb3435c59f470918f0a6ca145518e4
Nica Zulueta <nicacassandrazulueta@gmail.com>
2019-07-16 11:11:29 -0700

    working on update student

--- src/Students.js
+++ src/Students.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { destroyStudent } from './store'
+import { destroyStudent, updateStudent } from './store'
 
-const Students = ({students, deleteStudent}) => {
+const Students = ({schools, students, deleteStudent, enrollStudent}) => {
+  console.log(schools)
   return (
     <ul>
       {
@@ -11,8 +12,17 @@ const Students = ({students, deleteStudent}) => {
           <br />
           GPA: {student.gpa}
           <br />
-          <select name='enrollAt'>
+          <select name='enrollAt' defaultValue={student.enrollAt}
+              onChange={event =>
+                enrollStudent({
+                  enrollAt: event.target.value,
+                  studentId: student.id
+                })
+              }>
               <option>Not Enrolled</option>
+              {schools.schools.map(school => <option key={school.id} value={school.id}>
+                  {school.name}
+                </option>)}
             </select>
           <br />
           <button onClick={() => deleteStudent(student.id)}>Destroy Student</button>
@@ -22,17 +32,18 @@ const Students = ({students, deleteStudent}) => {
   );
 };
 
-const mapStateToProps = ({ students })=> {
+const mapStateToProps = ({ schools, students, match})=> {
     return {
-      students
+      students,
+      schools,
+      match
     }
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    deleteStudent: event => {
-      dispatch(destroyStudent(event));
-    }
+    deleteStudent: event => {dispatch(destroyStudent(event))},
+    enrollStudent: event => {dispatch(updateStudent(event))}
   }
 }
 
